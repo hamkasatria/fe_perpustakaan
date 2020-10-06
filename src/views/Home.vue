c45102942789d3358ef0a357a18f0dfec2f6398f
hamkasatria <muhhamkasatrianto@gmail.com>
2020-10-06 21:01:56 +0700

    menambahkan axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    menjelaskan tentang perpustakaan praxis secara umum
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+// export default {
+//   name: 'Home',
+//   components: {
+//     HelloWorld
+//   }
+// }
 </script>
--- src/views/Katalog.vue
+++ src/views/Katalog.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="katalog">
     <h1>This is an KATALOG page</h1>
+
+     <ul v-if="posts && posts.length">
+    <li v-for="post of posts" v-bind:key="post.id">
+      <p><strong>{{post.judul}}</strong></p>
+      <p>{{post.author}} - {{post.tahun}}</p>
+      <p>{{post.sinopsis}}</p>
+    </li>
+  </ul>
+
+  <ul v-if="errors && errors.length">
+    <li v-for="error of errors" v-bind:key="error.id">
+      {{error.message}}
+    </li>
+  </ul>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  created() {
+    axios.get(`http://localhost:8081/guess/k/`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }
+}
+</script>
